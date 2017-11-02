490c63f092d12a667529a8f7b5fa0ad2a823a15c
shaozhenyu <18217258834@163.com>
2017-11-02 10:52:27 +0800

    add

---
+++ src/rry/server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+
+	"ember/cli"
+	"ember/http/rpc"
+)
+
+type Server struct {
+	lock sync.Mutex
+}
+
+type Client struct {
+	Gets func(path string) (err error)               `args:"path" return:"err"`
+	Test func(path string) (value string, err error) `args:"path" return:"value,err"`
+}
+
+func Reg(cmds *cli.Cmds) {
+	cmds.Reg("run", "run slsync server", CmdRun)
+}
+
+func CmdRun(args []string) {
+	dir := "data"
+	sobj, err := NewServer(dir)
+	cli.Check(err)
+	rpc := rpc.NewServer()
+	err = rpc.Reg(sobj, &Client{})
+	cli.Check(err)
+	err = rpc.Run("/", 8080)
+	cli.Check(err)
+}
+
+func (p *Server) Gets(path string) (err error) {
+	fmt.Println("gets ", path)
+	return
+}
+
+func (p *Server) Test(path string) (string, error) {
+	fmt.Println("server test:", path)
+	return path + "11", nil
+}
+
+func NewServer(path string) (*Server, error) {
+	return &Server{}, nil
+}
